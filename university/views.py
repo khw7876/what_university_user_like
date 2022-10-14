c0bf9a9f07a2efcf4459d57ba4522429db54bfa9
khw7876 <khw7876@gmail.com>
2022-10-14 19:52:57 +0900

    feat/ 대학교 Json 데이터들을 model에 저장하는 로직 구현

--- university/views.py
+++ university/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 import requests, json
 
-from .models import Country
+from .models import Country, University
 # Create your views here.
 
 class UnivercityView(APIView):
@@ -24,6 +24,12 @@ class UnivercityView(APIView):
                 country_code_list.append(univercity_data['alpha_two_code'])
         for index, A in enumerate (country_name_list): 
             Country.objects.get_or_create(name = A, code = country_code_list[index])
-    
 
-        return Response({"detail": "회원가입을 성공하였습니다"}, status=status.HTTP_200_OK)
+        for univercity_data in data:
+            country_id = Country.objects.get(name = univercity_data["country"])
+            if University.objects.filter(name=univercity_data["name"]):
+                pass
+            else:
+                University.objects.get_or_create(name=univercity_data["name"], webpage = univercity_data["web_pages"][0], country=country_id)
+
+        return Response({"detail": "정보 저장이 완료되었습니다."}, status=status.HTTP_200_OK)
