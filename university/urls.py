790526e065e521baa6f579d4fe3ba234e31d7217
khw7876 <khw7876@gmail.com>
2022-10-14 19:28:52 +0900

    feat/ country 생성 로직 구현

--- university/urls.py
+++ university/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.UnivercityView.as_view(), name="universiry_view"),
 ]
--- university/views.py
+++ university/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
+import requests, json
 
+from .models import Country
 # Create your views here.
+
+class UnivercityView(APIView):
+    """
+    대학교에 관한 View
+    """
+    def get(self, request: Request) -> Response:
+        url = requests.get("http://universities.hipolabs.com/search")
+        text = url.text
+        data = json.loads(text)
+        country_name_list = []
+        country_code_list = []
+        
+        for univercity_data in data:
+            if univercity_data["country"] not in country_name_list:
+                country_name_list.append(univercity_data["country"])
+                country_code_list.append(univercity_data['alpha_two_code'])
+        for index, A in enumerate (country_name_list): 
+            Country.objects.get_or_create(name = A, code = country_code_list[index])
+    
+
+        return Response({"detail": "회원가입을 성공하였습니다"}, status=status.HTTP_200_OK)
